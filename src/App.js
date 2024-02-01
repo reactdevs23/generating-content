05a08f63d9dbb41252ec9a0d6367657330863813
dalimkumar604 <dalimkumar212582@gmail.com>
2024-02-01 10:42:42 +0600

    dsfask

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import { useState } from "react";
 import MainComponent from "./components/MaiComponent/MainComponent";
 import { img } from "./images";
 
 function App() {
-  const allData = {
+  const [allData, setAllData] = useState({
     fontFamily: "'Signika', sans-serif",
-    primaryColor: "#000",
-    secondaryColor: "#fff",
+    textColor: "#000",
+    numbersColor: "#000",
+    bgColor: "#fff",
     arrowColor: "#C1B1AD",
     heading: "4 Steps for Generating Content",
     data: [
       {
         img: img,
-        title: "The Research Phase",
-        info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
-        bg: "#FDCFB1",
+        title: "The Research Phase ",
+        info: `There are many  companies in today's market that have "maskots" who act as icons for there overall brand image`,
+        idWrapperBg: "#FDCFB1",
       },
       {
         img: img,
@@ -21,28 +23,30 @@ function App() {
         title: "Selecting The Platform",
         info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
 
-        bg: "#FDCFB1",
+        idWrapperBg: "#FDCFB1",
       },
       {
         img: img,
 
         title: "Creation of Content",
         info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
-        bg: "#FDCFB1",
+        idWrapperBg: "#FDCFB1",
       },
       {
         img: img,
-        title: "Analyzing the Data",
-        info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
-        bg: "#FF9772",
+        title: "Analyzing the Data ",
+        info: `There are many companies in today's market that `,
+        idWrapperBg: "#FF9772",
       },
     ],
-  };
+  });
   return (
     <div
       style={{
-        "--primaryColor": allData.primaryColor,
-        "--secondaryColor": allData.secondaryColor,
+        "--bgColor": allData.bgColor,
+        "--textColor": allData.textColor,
+        "--numberColor": allData.numbersColor,
+
         fontFamily: allData.fontFamily,
       }}
     >
--- src/components/MaiComponent/MainComponent.js
+++ src/components/MaiComponent/MainComponent.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styles from "./MainComponent.module.css";
 import IdContainer from "../../images/IdContainer";
 import Dirction from "../../images/Dirction";
 const MainComponent = ({ subHeading, heading, data, arrowColor }) => {
+  const titleRefs = useRef([]);
+
+  useEffect(() => {
+    // Get the maximum height among all title elements
+    const titles = titleRefs.current.map(
+      (ref) => ref.getBoundingClientRect().height
+    );
+    const maxTitleHeight = Math.max(...titles);
+
+    // Set the maximum height to all title elements using the titleRefs
+    titleRefs.current.forEach((ref) => {
+      ref.style.height = `${maxTitleHeight}px`;
+    });
+  }, [data]);
   return (
     <div className={styles.mainWrapper}>
       <div className={styles.wrapper}>
@@ -13,7 +27,6 @@ const MainComponent = ({ subHeading, heading, data, arrowColor }) => {
         <div className={styles.cards}>
           {data.map((el, id) => (
             <div
-              style={{ "--bg": el.bg }}
               key={id}
               className={`${styles.card} ${styles[`items-${data.length}`]}  ${
                 data.length > 5 && styles[`items-5`]
@@ -22,21 +35,28 @@ const MainComponent = ({ subHeading, heading, data, arrowColor }) => {
               {" "}
               <div className={styles.idAndDirection}>
                 <div className={styles.idContainer}>
-                  <IdContainer color={el.bg} />
+                  <IdContainer color={el.idWrapperBg} />
                   <p className={styles.id}>
                     {id + 1 < 10 ? `0${id + 1}` : id + 1}.
                   </p>
                 </div>
-                {data.length !== id + 1 && (
-                  <div className={styles.arrow}>
-                    <Dirction color={arrowColor} />
-                  </div>
-                )}
+
+                <div
+                  className={styles.arrow}
+                  style={{ visibility: data.length === id + 1 && "hidden" }}
+                >
+                  <Dirction color={arrowColor} />
+                </div>
               </div>
               <div className={styles.imgContainer}>
                 <img src={el.img} alt="#" className={styles.img} />
               </div>
-              <h4 className={styles.title}>{el.title}</h4>
+              <h4
+                ref={(el) => (titleRefs.current[id] = el)}
+                className={styles.title}
+              >
+                {el.title}
+              </h4>
               <p className={styles.info}>{el.info}</p>
             </div>
           ))}
